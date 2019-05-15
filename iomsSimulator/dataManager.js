8618869f5aecbf2a50e9aa76f130a673a17a2120
zhangkai <zhang.kai@kedacom.com>
2019-05-15 09:35:12 +0800

    modified dataManger.js

--- iomsSimulator/dataManager.js
+++ iomsSimulator/dataManager.js
@@ -1,11 +1,14 @@
 //var PAGE_COUNT = 10; //分页的记录数
 var fs = require("fs")
+var path = require("path")
 
 var dataMgr = function(){
 	/*this.PAGECOUNT = PAGE_COUNT;
 	this.redisContext = require('./redisAccess'); 
 	this.onvif = require('./onvifWsManager');*/
 }
+
+var platinfofile = path.join(__dirname,'platinfo.json');
 
 //初始化数据
 dataMgr.prototype.initData = function()
@@ -16,7 +19,7 @@ dataMgr.prototype.initData = function()
 //保存平台信息
 dataMgr.prototype.savePlatInfo = function(pi)
 {
-	fs.writeFile('platinfo.json',JSON.stringify(pi),function(err) 
+	fs.writeFile(platinfofile,JSON.stringify(pi),function(err) 
 	  {
          if (err) {
            return console.error(err);
@@ -29,7 +32,8 @@ dataMgr.prototype.savePlatInfo = function(pi)
 //载入平台信息
 dataMgr.prototype.loadPlatInfo = function(res)
 {
-	fs.stat('platinfo.json',function(err,stats){
+	console.log("current datafile is:",platinfofile);
+	fs.stat(platinfofile,function(err){
 	      if(err){//文件不存在
 			 console.log(err);
 			 res.write(JSON.stringify({
@@ -40,7 +44,7 @@ dataMgr.prototype.loadPlatInfo = function(res)
 			return res.end();
 									
 		  }else{
-			 var data = fs.readFileSync('platinfo.json'); 
+			 var data = fs.readFileSync(platinfofile); 
 			 console.log("read data from platinfo.json.");
 			 res.write(data.toString());
 			 return res.end();
